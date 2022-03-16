2df7c74cb0b70223248738b732b882d70e190549
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2022-03-16 18:55:52 +0500

    Initial_Commit"

---
+++ numPy.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a=np.array(range(12))
+b=np.array(range(12))
+c=a**b
+print(c)
---
+++ panga1.py
@@ -0,0 +1,16 @@
+file_loc="assets/color_text.txt"
+f = open(file_loc, "a")
+f.write(' -------------------------------\n')
+for i in range(32,37):
+    if(i==32):
+        f.write('| ')
+    f.write(f'\033[1;{i}mColor ')
+    if(i==36):
+        f.write('\033[1;37m|\n')
+
+f.write(' -------------------------------\n') 
+f.close()
+
+#open and read the file after the appending:
+f = open(file_loc, "r")
+print(f.read())
